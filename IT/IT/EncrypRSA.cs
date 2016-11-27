b3c09a97a30ef8b3e05022cf55bb0d269f4beaa3
YaValera <mamayavinternete@gmail.com>
2016-11-27 22:22:44 +0200

    Add Decryp algoritm

--- IT/IT/EncrypRSA.cs
+++ IT/IT/EncrypRSA.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace IT
 {
-    class EncrypRSA
+    class Change : Interface1
     {
+        BigInteger P, Q, funE;
+        Random rand = new Random();
+
+        public BigInteger GlobalKey { get; set; }
+
+        public BigInteger PrivateKey { get; set; }
+
+        public BigInteger PublicKey { get; set; }
+
+        public void CreateKey()
+        {
+            P = EncrypRSA.findSimpl(rand);
+            int random_key = 0;
+            Q = EncrypRSA.findSimpl(rand, 100, 200);
+            GlobalKey = P * Q;
+            funE = (P - 1) * (Q - 1);
+
+            PrivateKey = EncrypRSA.findprKey(funE);
+            PublicKey = EncrypRSA.findpubKey(PrivateKey, funE);
+        }
+
+        public string Encryp(string str)
+        {
+            BigInteger[] Input_str = new BigInteger[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+                Input_str[i] = MyPow(str[i], PublicKey, GlobalKey);
+
+            string Result_str = new string(Input_str.Select(o => (char)o).ToArray());
+
+            return Result_str;
+        }
+
+        public string Decryp(string str)
+        {
+            BigInteger[] Input_str = new BigInteger[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+                Input_str[i] = MyPow(str[i], PrivateKey, GlobalKey);
+
+            string Result_str = new string(Input_str.Select(o => (char)o).ToArray());
+
+            return Result_str;
+        }
+
+        private BigInteger MyPow(long x, BigInteger y, BigInteger k)
+        {
+            BigInteger num = new BigInteger(1);
+
+            for (int i = 0; i < y; i++)
+                num *= x % k;
+
+            return num % k;
+        }
+
     }
 }
